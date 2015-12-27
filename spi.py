4dc2a20b144efecd7423e15456454d7107c52a95
sunlx688 <sunlx688@163.com>
2015-12-27 21:07:03 +0800

    spi.py

---
+++ spi.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import requests, re
+import os
+from bs4 import BeautifulSoup
+
+url = 'https://mm.taobao.com/self/aiShow.htm?spm=719.7763510.1998643336.1.9zAfIW&userId=56540137'
+login_url = 'https://login.taobao.com/member/login.jhtml?redirectURL=https%3A%2F%2Fwww.taobao.com%2F'
+
+session = requests.session()
+session.post(login_url, {'username': '星火312', 'password': '5A4m8Y3c'})
+s = session.get(url)
+html = BeautifulSoup(s.text)
+
+urls = html.find_all(name="img", attrs={"style": re.compile(r'float: none;.？*')})
+for url in urls:
+    url = 'http:' + url['src']
+    print(url)
+# print(s.text)
